abbc40080ebaff2a0d3111c1593c9925710bdbd1
Tom <tnorth93@gmail.com>
2018-10-15 13:59:07 -0700

    app is working

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import Header from '../header/header';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.state.content = '';
+  }
+
+  render() {
+    return (
+      <div className='normal-class'>
+        <Header/> {}
+        <h2> HI</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
